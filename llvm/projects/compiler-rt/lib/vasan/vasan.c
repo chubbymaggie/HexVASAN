a4eefe14c1702f7cb8c010ceb12a26c93fb464df
Stijn Volckaert <stijnv@gmail.com>
2017-02-14 17:29:59 -0800

    Aggressive inlining for extra speedz

--- llvm/projects/compiler-rt/lib/vasan/vasan.c
+++ llvm/projects/compiler-rt/lib/vasan/vasan.c
@@ -5,23 +5,133 @@
 #ifndef NO_BACKTRACE
 #include <execinfo.h>
 #endif
-#include "vasan.h"
-#include "stack.h"
-#include "hashmap.h"
-
-#ifdef __FreeBSD__
-# include <sys/thr.h> // for thr_self
-#else
-# include <unistd.h>  // for syscall
-# ifdef __x86_64__
-#  include <asm/unistd_64.h> // for __NR_gettid
-# else
-#  include <asm/unistd_32.h>
-# endif
-#endif
+//#include "vasan.h"
+//#include "stack.h"
+//#include "hashmap.h"
 
 #define DEBUG
 #define MAXPATH 1000
+#define INITIAL_SIZE 1024
+#define MAP_MISSING -3  /* No such element */
+#define MAP_FULL -2 /* Hashmap is full */
+#define MAP_OMEM -1 /* Out of Memory */
+#define MAP_OK 0 /* OK */
+
+/*
+ * any_t is a pointer.  This allows you to put arbitrary structures in
+ * the hashmap.
+ */
+typedef void *any_t;
+
+/*
+ * PFany is a pointer to a function that can take two any_t arguments
+ * and return an integer. Returns status code..
+ */
+typedef int (*PFany)(any_t, any_t);
+
+/*
+ * map_t is a pointer to an internally maintained data structure.
+ * Clients of this package do not need to know how hashmaps are
+ * represented.  They see and manipulate only map_t's. 
+ */
+typedef any_t map_t;
+
+
+// this is what the instrumentation code will pass to the runtime.
+// it's type info that has not been associated with a va_list yet.
+struct vasan_type_info_tmp
+{
+	unsigned long id;
+	unsigned long arg_count;
+	unsigned long*  arg_array;	
+};
+
+struct vasan_type_info_full
+{
+	va_list* list_ptr;
+	unsigned int args_ptr;
+	struct vasan_type_info_tmp* types;
+};
+
+// We need to keep keys and values
+typedef struct _hashmap_element{
+	int key;
+	int in_use;
+	any_t data;
+} hashmap_element;
+
+// A hashmap has some maximum size and current size,
+// as well as the data to hold.
+typedef struct _hashmap_map{
+	int table_size;
+	int size;
+	hashmap_element *data;
+} hashmap_map;
+
+struct stack_elem_t
+{
+	void* data;
+	struct stack_elem_t* next;
+};
+
+struct stack_t
+{
+	struct stack_elem_t* top;
+};
+
+
+static inline unsigned char __vasan_stack_empty(struct stack_t* stack);
+static inline void __vasan_stack_push(struct stack_t* stack, void* data);
+static inline void* __vasan_stack_pop(struct stack_t* stack);
+static inline void* __vasan_stack_top(struct stack_t* stack);
+static inline struct stack_t* __vasan_stack_new();
+static inline void __vasan_stack_free(struct stack_t* stack);
+
+/*
+ * Return an empty hashmap. Returns NULL if empty.
+ */
+static inline map_t __vasan_hashmap_new();
+
+/*  
+ * Iteratively call f with argument (item, data) for 
+ * each element data in the hashmap. The function must
+ * return a map status code. If it returns anything other
+ * than MAP_OK the traversal is terminated. f must
+ * not reenter any hashmap functions, or deadlock may arise.
+ */
+static inline int __vasan_hashmap_iterate(map_t in, PFany f, any_t item);
+
+/*
+ * Add an element to the hashmap. Return MAP_OK or MAP_OMEM.
+ */
+static inline int __vasan_hashmap_put(map_t in, int key, any_t value);
+
+/*
+ * Get an element from the hashmap. Return MAP_OK or MAP_MISSING.
+ */
+static inline int __vasan_hashmap_get(map_t in, int key, any_t *arg);
+
+/*
+ * Remove an element from the hashmap. Return MAP_OK or MAP_MISSING.
+ */
+static inline int __vasan_hashmap_remove(map_t in, int key);
+
+/*
+ * Get any element. Return MAP_OK or MAP_MISSING.
+ * remove - should the element be removed from the hashmap
+ */
+static inline int __vasan_hashmap_get_one(map_t in, any_t *arg, int remove);
+
+/*
+ * Free the hashmap
+ */
+static inline void __vasan_hashmap_free(map_t in);
+
+/*
+ * Get the current size of a hashmap
+ */
+static inline int __vasan_hashmap_length(map_t in);
+
 
 // only holds the call site info
 static __thread struct stack_t* vasan_stack;
@@ -74,6 +184,330 @@ static void __vasan_unlock()
 	spinlock = 0;
 }
 
+static inline unsigned char __vasan_stack_empty(struct stack_t* stack)
+{
+	return (stack && stack->top) ? 1 : 0;
+}
+
+static inline void __vasan_stack_push(struct stack_t* stack, void* data)
+{
+	if (stack)
+	{
+		struct stack_elem_t* elem = (struct stack_elem_t*)malloc(sizeof(struct stack_elem_t));
+		elem->data = data;
+		elem->next = stack->top;
+		stack->top = elem;
+	}
+}
+
+static inline void* __vasan_stack_pop(struct stack_t* stack)
+{
+	if (stack && stack->top)
+	{
+		struct stack_elem_t* elem = stack->top;
+		void* result = elem->data;
+		stack->top = elem->next;
+		free(elem);		
+		return result;	    
+	}
+
+	return (void*)0;
+}
+
+static inline void* __vasan_stack_top(struct stack_t* stack)
+{
+	if (stack && stack->top)
+		return stack->top->data;
+	return (void*)0;		
+}
+
+static inline struct stack_t* __vasan_stack_new()
+{
+	struct stack_t* result = (struct stack_t*)malloc(sizeof(struct stack_t));
+	result->top = (void*)0;
+	return result;
+}
+
+static inline void __vasan_stack_free(struct stack_t* stack)
+{
+	if (stack)
+	{
+		while(stack->top)
+			(void)__vasan_stack_pop(stack);
+		free(stack);
+		stack = (struct stack_t*)0;
+	}
+}
+
+/*
+ * Return an empty hashmap, or NULL on failure.
+ */
+static inline map_t __vasan_hashmap_new() {
+	hashmap_map* m = (hashmap_map*) malloc(sizeof(hashmap_map));
+	if(!m) goto err;
+
+	m->data = (hashmap_element*) calloc(INITIAL_SIZE, sizeof(hashmap_element));
+	if(!m->data) goto err;
+
+	m->table_size = INITIAL_SIZE;
+	m->size = 0;
+
+	return m;
+err:
+	if (m)
+		__vasan_hashmap_free(m);
+	return NULL;
+}
+
+/*
+ * Hashing function for an integer
+ */
+static inline unsigned int __vasan_hashmap_hash_int(hashmap_map * m, unsigned int key){
+	/* Robert Jenkins' 32 bit Mix Function */
+	key += (key << 12);
+	key ^= (key >> 22);
+	key += (key << 4);
+	key ^= (key >> 9);
+	key += (key << 10);
+	key ^= (key >> 2);
+	key += (key << 7);
+	key ^= (key >> 12);
+
+	/* Knuth's Multiplicative Method */
+	key = (key >> 3) * 2654435761;
+
+	return key % m->table_size;
+}
+
+/*
+ * Return the integer of the location in data
+ * to store the point to the item, or MAP_FULL.
+ */
+static inline int __vasan_hashmap_hash(map_t in, int key){
+	int curr;
+	int i;
+
+	/* Cast the hashmap */
+	hashmap_map* m = (hashmap_map *) in;
+
+	/* If full, return immediately */
+	if(m->size == m->table_size) return MAP_FULL;
+
+	/* Find the best index */
+	curr = __vasan_hashmap_hash_int(m, key);
+
+	/* Linear probling */
+	for(i = 0; i< m->table_size; i++){
+		if(m->data[curr].in_use == 0)
+			return curr;
+
+		if(m->data[curr].key == key && m->data[curr].in_use == 1)
+			return curr;
+
+		curr = (curr + 1) % m->table_size;
+	}
+
+	return MAP_FULL;
+}
+
+/*
+ * Doubles the size of the hashmap, and rehashes all the elements
+ */
+static inline int __vasan_hashmap_rehash(map_t in){
+	int i;
+	int old_size;
+	hashmap_element* curr;
+
+	/* Setup the new elements */
+	hashmap_map *m = (hashmap_map *) in;
+	hashmap_element* temp = (hashmap_element *)
+		calloc(2 * m->table_size, sizeof(hashmap_element));
+	if(!temp) return MAP_OMEM;
+
+	/* Update the array */
+	curr = m->data;
+	m->data = temp;
+
+	/* Update the size */
+	old_size = m->table_size;
+	m->table_size = 2 * m->table_size;
+	m->size = 0;
+
+	/* Rehash the elements */
+	for(i = 0; i < old_size; i++){
+		int status = __vasan_hashmap_put(m, curr[i].key, curr[i].data);
+		if (status != MAP_OK)
+			return status;
+	}
+
+	free(curr);
+
+	return MAP_OK;
+}
+
+/* 
+ * Add a pointer to the hashmap with some key
+ */
+static inline int __vasan_hashmap_put(map_t in, int key, any_t value){
+	int index;
+	hashmap_map* m;
+
+	/* Cast the hashmap */
+	m = (hashmap_map *) in;
+
+	/* Find a place to put our value */
+	index = __vasan_hashmap_hash(in, key);
+	while(index == MAP_FULL){
+		if (__vasan_hashmap_rehash(in) == MAP_OMEM) {
+			return MAP_OMEM;
+		}
+		index = __vasan_hashmap_hash(in, key);
+	}
+
+	/* Set the data */
+	m->data[index].data = value;
+	m->data[index].key = key;
+	m->data[index].in_use = 1;
+	m->size++;
+
+	return MAP_OK;
+}
+
+/*
+ * Get your pointer out of the hashmap with a key
+ */
+static inline int __vasan_hashmap_get(map_t in, int key, any_t *arg){
+	int curr;
+	int i;
+	hashmap_map* m;
+
+	/* Cast the hashmap */
+	m = (hashmap_map *) in;
+
+	/* Find data location */
+	curr = __vasan_hashmap_hash_int(m, key);
+
+	/* Linear probing, if necessary */
+	for(i = 0; i< m->table_size; i++){
+
+		if(m->data[curr].key == key && m->data[curr].in_use == 1){
+			*arg = (int *) (m->data[curr].data);
+			return MAP_OK;
+		}
+
+		curr = (curr + 1) % m->table_size;
+	}
+
+	*arg = NULL;
+
+	/* Not found */
+	return MAP_MISSING;
+}
+
+/*
+ * Get a random element from the hashmap
+ */
+static inline int __vasan_hashmap_get_one(map_t in, any_t *arg, int remove){
+	int i;
+	hashmap_map* m;
+
+	/* Cast the hashmap */
+	m = (hashmap_map *) in;
+
+	/* On empty hashmap return immediately */
+	if (__vasan_hashmap_length(m) <= 0)
+		return MAP_MISSING;
+
+	/* Linear probing */
+	for(i = 0; i< m->table_size; i++)
+		if(m->data[i].in_use != 0){
+			*arg = (any_t) (m->data[i].data);
+			if (remove) {
+				m->data[i].in_use = 0;
+				m->size--;
+			}
+			return MAP_OK;
+		}
+
+	return MAP_OK;
+}
+
+/*
+ * Iterate the function parameter over each element in the hashmap.  The
+ * additional any_t argument is passed to the function as its first
+ * argument and the hashmap element is the second.
+ */
+static inline int __vasan_hashmap_iterate(map_t in, PFany f, any_t item) {
+	int i;
+
+	/* Cast the hashmap */
+	hashmap_map* m = (hashmap_map*) in;
+
+	/* On empty hashmap, return immediately */
+	if (__vasan_hashmap_length(m) <= 0)
+		return MAP_MISSING;
+
+	/* Linear probing */
+	for(i = 0; i< m->table_size; i++)
+		if(m->data[i].in_use != 0) {
+			any_t data = (any_t) (m->data[i].data);
+			int status = f(item, data);
+			if (status != MAP_OK) {
+				return status;
+			}
+		}
+
+	return MAP_OK;
+}
+
+/*
+ * Remove an element with that key from the map
+ */
+static inline int __vasan_hashmap_remove(map_t in, int key){
+	int i;
+	int curr;
+	hashmap_map* m;
+
+	/* Cast the hashmap */
+	m = (hashmap_map *) in;
+
+    /* Find key */
+	curr = __vasan_hashmap_hash_int(m, key);
+
+	/* Linear probing, if necessary */
+	for(i = 0; i< m->table_size; i++){
+		if(m->data[curr].key == key && m->data[curr].in_use == 1){
+			/* Blank out the fields */
+			m->data[curr].in_use = 0;
+			m->data[curr].data = NULL;
+			m->data[curr].key = 0;
+
+			/* Reduce the size */
+			m->size--;
+			return MAP_OK;
+		}
+		curr = (curr + 1) % m->table_size;
+	}
+
+	/* Data not found */
+	return MAP_MISSING;
+}
+
+/* Deallocate the hashmap */
+static inline void __vasan_hashmap_free(map_t in){
+	hashmap_map* m = (hashmap_map*) in;
+	free(m->data);
+	free(m);
+}
+
+/* Return the length of the hashmap */
+static inline int __vasan_hashmap_length(map_t in){
+	hashmap_map* m = (hashmap_map *) in;
+	if(m != NULL) return m->size;
+	else return 0;
+}
+
+
 // We have to refuse to initialize until TLS is active
 static unsigned char __vasan_init()
 {
@@ -117,7 +551,7 @@ static unsigned char __vasan_init()
     return 1;
 }
 
-void __attribute__((destructor)) __vasan_fini()
+static void __attribute__((destructor)) __vasan_fini()
 {
 	// TODO: Do a proper cleanup here
 	
